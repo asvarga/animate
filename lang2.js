99bd7cf7f445d53af2ac826c85db958d6503b028
thealexvarga <thealexvarga@gmail.com>
2017-07-01 17:12:56 -0400

    animation with interpretation test

--- lang2.js
+++ lang2.js
@@ -35,6 +35,20 @@ function FUNC(name, vars, body, env) { return new Func(name, vars, body, env); }
 
 add 	= (x,y) => x+y;
 mul 	= (x,y) => x*y;
+sub 	= (x,y) => x-y;
+div 	= (x,y) => x/y;
+quint	= (x) => x*x*x*(6*x*x - 15*x + 10);
+eq		= (x,y) => x==y;
+time 	= () => createjs.Ticker.getTime(true);
+
+function extend(base, vars, args) {
+	var extension = {};
+	for (var i=0; i<vars.length; i++) {
+		extension[vars[i]] = args[i];
+	}
+	extension.__proto__ = base;
+	return extension;
+}
 
 ////////
 
@@ -51,25 +65,48 @@ function interp(x, env) {
 		return FUNC(x.name, x.vars, x.body, env);
 	} else if (x instanceof Var) {
 		return env[x.name];
+	} else if (x instanceof If) {
+		var c = interp(x.cond, env);
+		if (c >= 1) { return interp(x.conseq, env); }
+		if (c <= 0) { return interp(x.altern, env); }
+		return c*interp(x.conseq, env) + (1-c)*interp(x.altern, env);
 	}
 	return x;
 }
 
-function extend(base, vars, args) {
-	var extension = {};
-	for (var i=0; i<vars.length; i++) {
-		extension[vars[i]] = args[i];
+function interp2(x, env) {
+	var env = env || {};
+	if (x instanceof App) {
+		var f = interp2(x.f, env);
+		var args = x.args.map(arg => interp2(arg, env));
+		if (f instanceof Func) { 
+			return interp2(f.body, extend(f.env, [f.name].concat(f.vars), [f].concat(args))); 
+		}
+		return f.apply(null, args);
+	} else if (x instanceof Func) {
+		return FUNC(x.name, x.vars, x.body, env);
+	} else if (x instanceof Var) {
+		return env[x.name];
+	} else if (x instanceof If) {
+		var c = interp2(x.cond, env);
+		if (c >= 1) { return interp2(x.conseq, env); }
+		if (c <= 0) { return interp2(x.altern, env); }
+		return c*interp2(x.conseq, env) + (1-c)*interp2(x.altern, env);
 	}
-	extension.__proto__ = base;
-	return extension;
+	return x;
 }
+
+
+// TODO: make Frame a new type for easier pruning
+
 
 ////////
 
-var test = APP(FUNC('f', ['x'], APP(mul, VAR('x'), VAR('x'))), APP(add, 4, 5));
-// var test = APP(add, 4, 5);
-console.log(interp(test));
-
+// var prog = APP(FUNC('f', ['x'], APP(mul, VAR('x'), VAR('x'))), APP(add, 4, 5));
+// var prog = APP(add, 4, 5);
+var prog = IF(APP(mul, APP(time), 0.001), 800, 200);
+console.log(interp(prog));
+console.log(JSON.stringify(prog));
 
 
 
--- main.js
+++ main.js
@@ -16,17 +16,17 @@ function load() {
 		// }
 	});
 
-	circles = [];
-	// for (var i=1000; i<=2000; i+=100) {
-	// 	addCircle(i);
-	// }
-	addCircle(1000);
+	// circles = [];
+	// // for (var i=1000; i<=2000; i+=100) {
+	// // 	addCircle(i);
+	// // }
+	// addCircle(1000);
 
-	MOUSE = new Point(500, 500);
-	stage.on("stagemousemove", function(evt) {
-		MOUSE.x = evt.stageX;
-		MOUSE.y = evt.stageY;
-	});
+	// MOUSE = new Point(500, 500);
+	// stage.on("stagemousemove", function(evt) {
+	// 	MOUSE.x = evt.stageX;
+	// 	MOUSE.y = evt.stageY;
+	// });
 
 	// this.document.onkeydown = function(evt) {
 	// 	// console.log(event.keyCode);
@@ -36,21 +36,32 @@ function load() {
 	// 			break;
 	// 	}
 	// };
+
+	circle = new createjs.Shape();
+	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 20);
+	var prog = IF(APP(quint, APP(mul, APP(time), 0.001)), 800, 200);
+	circle.x = {
+		valueOf: () => interp(prog)
+	};
+	// circle.x = 500;
+	circle.y = 500;
+
+	stage.addChild(circle);
 	
 
 	tick();
 }
 
-function addCircle(d) {
-	circle = new createjs.Shape();
-	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 20);
-	circle.x = 500;
-	circle.y = 500;
-	circle.d = d;
+// function addCircle(d) {
+// 	circle = new createjs.Shape();
+// 	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 20);
+// 	circle.x = 500;
+// 	circle.y = 500;
+// 	circle.d = d;
 
-	stage.addChild(circle);
-	circles.push(circle);
-} 
+// 	stage.addChild(circle);
+// 	circles.push(circle);
+// } 
 
 function resize() {
 	var w = window.innerWidth;
@@ -65,13 +76,13 @@ function resize() {
 
 function tick(evt={}) {
 	if (!evt.paused) {
-		for (var i=0; i<circles.length; i++) {
-			var circle = circles[i];
-		    circle.x = dual(circle.x, MOUSE.x, frame(circle.d));
-		    circle.y = dual(circle.y, MOUSE.y, frame(circle.d));
-		}
+	// 	for (var i=0; i<circles.length; i++) {
+	// 		var circle = circles[i];
+	// 	    circle.x = dual(circle.x, MOUSE.x, frame(circle.d));
+	// 	    circle.y = dual(circle.y, MOUSE.y, frame(circle.d));
+	// 	}
 		stage.update();
-		// console.log(depth(circle.x));
+	// 	// console.log(depth(circle.x));
 	}
 }
 
